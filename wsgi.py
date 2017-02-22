6d72c1e777f1823570664561a6edc7ce7334f675
Oved Ourfali <oourfali@redhat.com>
2017-02-22 16:36:12 +0200

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -118,4 +118,4 @@ api.add_resource(DataWithin, '/ws/data/within')
 
 @application.route('/')
 def index():
-    return 'Welcome to the National Parks data service. Oved!'
+    return 'Welcome to the National Parks data service. Oved Test!'
